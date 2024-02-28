d585ab558f0ba700375afa4f53e4badcf4cee965
Igor Cappello <igor.cappello@gmail.com>
2024-02-28 16:27:16 +0100

    add: basic rabbitmq installation scripts for Ubuntu

---
+++ cluster-install/install-scripts/setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#usage
+#   $> sudo setup.sh ["the-erlang-cookie"]
+#
+echo "---------------------"
+echo "Installing base tools"
+echo "---------------------"
+apt-get update -y
+apt-get install curl gnupg apt-transport-https net-tools -y
+
+echo "----------------------------"
+echo "Adding rabbitmq repositories"
+echo "----------------------------"
+./cloudsmith_repos.sh
+
+echo "-------------------------"
+echo "Indexing new repositories"
+echo "-------------------------"
+## Update package indices
+apt-get update -y
+
+echo "-----------------"
+echo "Installing erlang"
+echo "-----------------"
+## Install Erlang packages
+apt-get install -y erlang-base \
+                        erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets \
+                        erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \
+                        erlang-runtime-tools erlang-snmp erlang-ssl \
+                        erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl
+
+echo "-------------------"
+echo "Installing rabbitmq"
+echo "-------------------"
+## Install rabbitmq-server and its dependencies
+apt-get install rabbitmq-server -y --fix-missing
+
+echo "---------------------------"
+echo "Running rabbitmq at startup"
+echo "---------------------------"
+#start rabbitmq and make it run at startup
+systemctl enable rabbitmq-server
+
+echo "--------------------------"
+echo "Enabling management plugin"
+echo "--------------------------"
+#enable management plugin
+rabbitmq-plugins enable rabbitmq_management
+
+echo "-------------------"
+echo "Adding default user"
+echo "-------------------"
+rabbitmqctl add_user rabbit rabbit
+rabbitmqctl set_user_tags rabbit administrator
+rabbitmqctl set_permissions -p / rabbit ".*" ".*" ".*"
+
+#overwrite the erlang cookie
+./set_erlang_cookie.sh $1
---
+++ cluster-install/install-scripts/setup_cluster.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#First setup the node with setup.sh, then use this script to join a cluster.
+#usage
+#   $> sudo setup_cluster.sh "rabbit@the-cluster-node" "the-erlang-cookie"
+#
+SETUP_CLUSTER_HOST=$1
+SETUP_ERLANG_COOKIE=$2
+
+#overwrite the erlang cookie
+./set_erlang_cookie.sh $SETUP_ERLANG_COOKIE
+
+
+#prepare the local node to join the cluster
+echo "--------------------------------------------"
+echo "Preparing the local node to join the cluster"
+echo "--------------------------------------------"
+rabbitmqctl stop_app
+rabbitmqctl reset
+
+
+#join operation request
+echo "----------------------------------------------------------"
+echo "Issuing cluster join request to node ${SETUP_CLUSTER_HOST}"
+echo "----------------------------------------------------------"
+rabbitmqctl join_cluster $SETUP_CLUSTER_HOST
+
+#start node apps
+echo "------------------------------"
+echo "Starting internal rabbitmq app"
+echo "------------------------------"
+rabbitmqctl start_app
